2390c5060f2aa0d769f35cbf73cff55aa488273c
Steven O'Toole <github@o2l.com>
2015-04-15 16:58:20 -0700

    get session from Udacity API

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        loginTextField.text = ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   @IBAction func doLogin() {
+        if let userName = loginTextField.text, let password = passwordTextField.text {
+            let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+            request.HTTPMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = "{\"udacity\": {\"username\": \"\(userName)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
 
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request) { data, response, error in
+                if error != nil {
+                    println("Failed to get Udacity session: \(error)")
+                    return
+                }
+                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+                println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            }
+            task.resume()
+        }
+    }
 
 }
 
