93136ef882ad8b438ba4de7491850650eb5845ae
Steven O'Toole <github@o2l.com>
2015-04-17 09:31:45 -0700

    remove obsolete refactored code

--- OnTheMap/NetClient.swift
+++ OnTheMap/NetClient.swift
@@ -8,23 +8,22 @@
 
 import Foundation
 
-public typealias RequestClosure = (result: AnyObject!, error: NSError?) -> Void
 public typealias TaskRequestClosure = (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void
 
 
 class NetClient {
 
-    let PARSE_API_STUDENT_LOCATIONS_URL = "https://api.parse.com/1/classes/StudentLocation"
-    let PARSE_API_APP_ID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
-    let PARSE_API_REST_KEY = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    private let UDACITY_API_SESSION_URL = "https://www.udacity.com/api/session"
+    private let UDACITY_API_USERS_URL = "https://www.udacity.com/api/users/"
+    private let PARSE_API_STUDENT_LOCATIONS_URL = "https://api.parse.com/1/classes/StudentLocation"
+    private let PARSE_API_APP_ID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+    private let PARSE_API_REST_KEY = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
 
-    let session = NSURLSession.sharedSession()
-
-
+    private let session = NSURLSession.sharedSession()
 
 
     func loadSessionIDAndUserKey(userName: String, password: String, completionHandler: TaskRequestClosure) {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: UDACITY_API_SESSION_URL)!)
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -33,6 +32,11 @@ class NetClient {
         task.resume()
     }
 
+    func loadPublicUserData(userKey: String, completionHandler: TaskRequestClosure) {
+        let request = NSMutableURLRequest(URL: NSURL(string: UDACITY_API_USERS_URL + "\(userKey)")!)
+        let task = session.dataTaskWithRequest(request, completionHandler: completionHandler)
+        task.resume()
+    }
 
     func loadStudentLocations(completionHandler: TaskRequestClosure) {
         let request = NSMutableURLRequest(URL: NSURL(string: PARSE_API_STUDENT_LOCATIONS_URL)!)
@@ -41,106 +45,5 @@ class NetClient {
         let task = session.dataTaskWithRequest(request, completionHandler: completionHandler)
         task.resume()
     }
-
-
-
-//    public func loadStudentLocations(completionHandler: NSURLSessionDataTask) {
-//        let request = NSMutableURLRequest(URL: NSURL(string: PARSE_API_STUDENT_LOCATIONS_URL)!)
-//        request.addValue(PARSE_API_APP_ID, forHTTPHeaderField: "X-Parse-Application-Id")
-//        request.addValue(PARSE_API_REST_KEY, forHTTPHeaderField: "X-Parse-REST-API-Key")
-//        let session = NSURLSession.sharedSession()
-//        let task = session.dataTaskWithRequest(request) { data, response, error in
-//            if error != nil { // Handle error...
-//                println("Failed to get Parse API student location data: \(error)")
-//                return
-//            }
-//            //    println(NSString(data: data, encoding: NSUTF8StringEncoding))
-//            var parseError: NSError? = nil
-//            let topDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
-//            if let error = parseError {
-//                println("Failed to parse Parse data: \(error)")
-//            } else {
-//                if let userDict = topDict!["results"] as? [[String: AnyObject]] {
-//                    self.studentManager = StudentManager(dictionary: userDict)
-//                    println("\(self.studentManager)")
-//                    //  dispatch_async(dispatch_get_main_queue(), {  })
-//                } else {
-//                    println("Failed to find user dictionary")
-//                }
-//            }
-//        }
-//        task.resume()
-//    }
-
-
-
-
-//
-//    func taskForRequest(urlRequest: NSURLRequest, completionHandler: RequestClosure) -> NSURLSessionDataTask {
-//
-//        let task = session.dataTaskWithRequest(urlRequest) {data, response, downloadError in
-//
-//            /* 5/6. Parse the data and use the data (happens in completion handler) */
-//            if let error = downloadError {
-//                let newError = NetClient.errorForData(data, response: response, error: error)
-//                completionHandler(result: nil, error: downloadError)
-//            } else {
-//                NetClient.parseJSONWithCompletionHandler(data, completionHandler: completionHandler)
-//            }
-//        }
-//
-//        /* 7. Start the request */
-//        task.resume()
-//        return task
-//    }
-//
-    /* Helper: Given a response with error, see if a status_message is returned, otherwise return the previous error */
-//    class func errorForData(data: NSData?, response: NSURLResponse?, error: NSError) -> NSError {
-//
-//        if let parsedResult = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil) as? [String : AnyObject] {
-//
-//            if let errorMessage = parsedResult[TMDBClient.JSONResponseKeys.StatusMessage] as? String {
-//
-//                let userInfo = [NSLocalizedDescriptionKey : errorMessage]
-//
-//                return NSError(domain: "TMDB Error", code: 1, userInfo: userInfo)
-//            }
-//        }
-//
-//        return error
-//    }
-//
-//
-//
-//
-//    func taskForGETMethod(method: String, parameters: [String: AnyObject], completionHandler: RequestClosure) -> NSURLSessionDataTask {
-//
-//        /* 1. Set the parameters */
-//        var mutableParameters = parameters
-//        mutableParameters[ParameterKeys.ApiKey] = Constants.ApiKey
-//
-//        /* 2/3. Build the URL and configure the request */
-//        let urlString = Constants.BaseURLSecure + method + TMDBClient.escapedParameters(mutableParameters)
-//        let url = NSURL(string: urlString)!
-//        let request = NSURLRequest(URL: url)
-//
-//        /* 4. Make the request */
-//        let task = session.dataTaskWithRequest(request) {data, response, downloadError in
-//
-//            /* 5/6. Parse the data and use the data (happens in completion handler) */
-//            if let error = downloadError {
-//                let newError = TMDBClient.errorForData(data, response: response, error: error)
-//                completionHandler(result: nil, error: downloadError)
-//            } else {
-//                TMDBClient.parseJSONWithCompletionHandler(data, completionHandler: completionHandler)
-//            }
-//        }
-//
-//        /* 7. Start the request */
-//        task.resume()
-//
-//        return task
-//    }
-
 
 }
--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -31,47 +31,32 @@ class ViewController: UIViewController {
         passwordTextField.leftView = passSpacer
     }
 
+
+    // MARK: -
+    // MARK: login-related functions
+
     @IBAction func doLogin() {
         if let userName = loginTextField.text, let password = passwordTextField.text {
             self.loadSessionIDAndUserKey(userName, password: password)
         }
     }
-    
 
-/*
-    private func loadSessionIDAndUserKey(userName: String, password: String) {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(userName)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil {
-                println("Failed to get Udacity session: \(error)")
-                return
-            }
-            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))  /* subset response data! */
-            var parseError: NSError? = nil
-            let topDict = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
-            if let error = parseError {
-                println("Failed to get parse Udacity data: \(error)")
-            } else {
-                if let accountDict = topDict!["account"] as? NSDictionary, let sessionDict = topDict!["session"] as? NSDictionary {
-                    self.userKey = accountDict["key"] as? String
-                    self.sessionID = sessionDict["id"] as? String
-                          println("\(self.userKey)\n\(self.sessionID)")
-                    dispatch_async(dispatch_get_main_queue(), { self.loadPublicUserData() })
-                }
-            }
-        }
-        task.resume()
-    }
-*/
     private func loadSessionIDAndUserKey(userName: String, password: String) {
         netClient.loadSessionIDAndUserKey(userName, password: password, completionHandler: sessionAndUserKeyClosure)
     }
+
+    private func loadPublicUserData() {
+        if self.currentUser.userKey == nil { println("loadPublicUserData but userKey is nil"); return }
+        netClient.loadPublicUserData(self.currentUser.userKey!, completionHandler: publicUserDataClosure)
+    }
+
+    private func loadStudentLocations() {
+        netClient.loadStudentLocations(studentLocationClosure)
+    }
+
+
+    // MARK: -
+    // MARK: login-related closures
 
     func sessionAndUserKeyClosure(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void {
         if error != nil {
@@ -82,7 +67,7 @@ class ViewController: UIViewController {
         var parseError: NSError? = nil
         let topDict = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
         if let error = parseError {
-            println("Failed to get parse Udacity data: \(error)")
+            println("Failed to parse Udacity data: \(error)")
         } else {
             if let accountDict = topDict!["account"] as? NSDictionary, let sessionDict = topDict!["session"] as? NSDictionary {
                 self.currentUser.userKey = accountDict["key"] as? String
@@ -93,37 +78,28 @@ class ViewController: UIViewController {
         }
     }
 
-
-
-    private func loadPublicUserData() {
-        if self.currentUser.userKey == nil { println("loadPublicUserData but userKey is nil"); return }
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/users/\(self.currentUser.userKey!)")!)
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error...
-                println("Failed to get Udacity public user data: \(error)")
-                return
-            }
-            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-            var parseError: NSError? = nil
-            let topDict = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
-            if let error = parseError {
-                println("Failed to parse Udacity data: \(error)")
-            } else {
-                if let userDict = topDict!["user"] as? NSDictionary {
-                    self.currentUser.lastName = userDict["last_name"] as? String
-                    self.currentUser.firstName = userDict["first_name"] as? String
-                    if let emailDict = userDict["email"] as? NSDictionary {
-                        self.currentUser.email = emailDict["address"] as? String
-                    }
-                    //   println("\(self.firstName)\n\(self.lastName)\n\(self.email)")
-                    //            dispatch_async(dispatch_get_main_queue(), { self.loadStudentLocations() })
+    func publicUserDataClosure(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void {
+        if error != nil { // Handle error...
+            println("Failed to get Udacity public user data: \(error)")
+            return
+        }
+        let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+        var parseError: NSError? = nil
+        let topDict = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
+        if let error = parseError {
+            println("Failed to parse Udacity data: \(error)")
+        } else {
+            if let userDict = topDict!["user"] as? NSDictionary {
+                self.currentUser.lastName = userDict["last_name"] as? String
+                self.currentUser.firstName = userDict["first_name"] as? String
+                if let emailDict = userDict["email"] as? NSDictionary {
+                    self.currentUser.email = emailDict["address"] as? String
                 }
+                //   println("\(self.firstName)\n\(self.lastName)\n\(self.email)")
+                //            dispatch_async(dispatch_get_main_queue(), { self.loadStudentLocations() })
             }
         }
-        task.resume()
     }
-
 
     func studentLocationClosure(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void {
         if error != nil { // Handle error...
@@ -146,38 +122,6 @@ class ViewController: UIViewController {
         }
     }
 
-    private func loadStudentLocations() {
-        netClient.loadStudentLocations(studentLocationClosure)
-    }
-/*
-    private func loadStudentLocations() {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
-        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error...
-                println("Failed to get Parse API student location data: \(error)")
-                return
-            }
-            //    println(NSString(data: data, encoding: NSUTF8StringEncoding))
-            var parseError: NSError? = nil
-            let topDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as? NSDictionary
-            if let error = parseError {
-                println("Failed to parse Parse data: \(error)")
-            } else {
-                if let userDict = topDict!["results"] as? [[String: AnyObject]] {
-                    self.studentManager = StudentManager(dictionary: userDict)
-                    println("\(self.studentManager)")
-                    //  dispatch_async(dispatch_get_main_queue(), {  })
-                } else {
-                    println("Failed to find user dictionary")
-               }
-            }
-        }
-        task.resume()
-    }
-*/
 
 }
 
